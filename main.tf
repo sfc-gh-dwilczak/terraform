0925573152b82fe4e8d4bc9d21549326624d4ac7
Daniel Wilczak <141693938+sfc-gh-dwilczak@users.noreply.github.com>
2023-08-23 17:45:32 -0500

    Updated path

--- main.tf
+++ main.tf
@@ -26,5 +26,5 @@ provider "snowflake" {
 module "fivetran_resources" {
   name    = "FIVETRAN_DATABASE"
   comment = "Fivetran database for loading in data."
-  source  = "modules/fivetran_resources"
+  source  = "./modules/fivetran_resources"
 }
